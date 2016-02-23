8af65f7e0193550feafe36b3b278a23ff7bfae48
AaronDRS <aabuckel@gmail.com>
2016-02-22 23:02:13 -0600

    unnecessary lines deleted

--- js/main.js
+++ js/main.js
@@ -37,9 +37,8 @@ var app = (function(window, undefined){
          dataType: 'json',
          success: function(data) {
             console.log(data.data);
-            console.log();
             var lineChartData = {
-                labels: [data.data[0].date, data.data[1].date, data.data[2].date, data.data[3].date,  "June", "July"],
+                labels: [],
                 datasets: [{
                     label: "My First dataset",
                     fillColor: "rgba(220,220,220,0.2)",
@@ -48,19 +47,11 @@ var app = (function(window, undefined){
                     pointStrokeColor: "#fff",
                     pointHighlightFill: "#fff",
                     pointHighlightStroke: "rgba(220,220,220,1)",
-                    data: [65, 59, 80, 81, 56, 55, 40]
-                }, {
-                    label: "My Second dataset",
-                    fillColor: "rgba(151,187,205,0.2)",
-                    strokeColor: "rgba(151,187,205,1)",
-                    pointColor: "rgba(151,187,205,1)",
-                    pointStrokeColor: "#fff",
-                    pointHighlightFill: "#fff",
-                    pointHighlightStroke: "rgba(151,187,205,1)",
-                    data: [28, 48, 40, 19, 86, 27, 90]
+                    data: [90, 80, 70, 60, 50]                
                 }]
             };
-
+            lineChartData.labels.push('hola');
+            console.log(lineChartData.labels);
             window.myLine = new Chart(ctx).Line(lineChartData, {
                 responsive: true
             });
